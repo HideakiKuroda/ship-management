8f3eeca3de2e0ee3aa0360286110023b629ba81f
HideakiKuroda <ntb.kurodah@gmail.com>
2023-10-28 23:37:52 +0900

    app.blade.php

--- resources/js/http-client.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const csrfToken = document.querySelector('meta[name="csrf-token"]').getAttribute('content');
-
-// AxiosのデフォルトヘッダにX-CSRF-TOKENを設定
-axios.defaults.headers.common['X-CSRF-TOKEN'] = csrfToken;
-
-export default axios;
